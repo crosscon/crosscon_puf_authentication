72cc6f793609e47a49d5619f22346b26e971581c
Tymoteusz Burak <tymoteusz.burak@uni-wuerzburg.de>
2025-02-20 12:07:50 +0100

    *: dont track artifacts and settings + gitignore

--- LPC55S69_PAVOC/Debug/freertos/corejson/source/core_json.d
+++
@@ -1,4 +0,0 @@
-freertos/corejson/source/core_json.o freertos/corejson/source/core_json.d: \
- ../freertos/corejson/source/core_json.c \
- C:\Users\lup22vs\Desktop\CROSSCON\PUF-authentication\LPC55S69_PAVOC\freertos\corejson\source\include/core_json.h
-C:\Users\lup22vs\Desktop\CROSSCON\PUF-authentication\LPC55S69_PAVOC\freertos\corejson\source\include/core_json.h:
